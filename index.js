c9918905acff2a70ece27db4ec256fc261dc1e16
Naukhez Ali <naukhez.3@hotmail.co.uk>
2021-06-12 11:13:56 +0100

    seperated out app and server

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 
 let ipDB = {};
 
-app.get("/", (req, res) => {
+app.get("/subscribe", (req, res) => {
     // Get the IP address of the caller
     const usersIpAddress =
         req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+
+    // Delete this line
     console.log("usersIpAddress: ", usersIpAddress);
 
     // Check if the IP address is stored already
     if (usersIpAddress in ipDB) {
+        // Increment the number of streams associated with that IP address
         ipDB[usersIpAddress] += 1;
 
         let totalNumberOfStreams = ipDB[usersIpAddress];
@@ -42,6 +44,32 @@ app.get("/", (req, res) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+app.get("/unsubscribe", (req, res) => {
+    // Get the IP address of the caller
+    const usersIpAddress =
+        req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+
+     // Delete this line
+     console.log("usersIpAddress: ", usersIpAddress);
+
+    // Check if the IP address is stored already
+    if (usersIpAddress in ipDB && ipDB[usersIpAddress] > 0) {
+        // Decrement the number of streams associated with that IP address
+        ipDB[usersIpAddress] -= 1;
+        res.json({
+            statusCode: 200,
+            message:
+                "You have unsubscribed successfully.",
+            status: "success",
+        });
+    } else {
+        res.json({
+            statusCode: 200,
+            message:
+                "This device has not subscribed to this service.",
+            status: "failed",
+        });
+    }
 });
+
+module.exports = app;
